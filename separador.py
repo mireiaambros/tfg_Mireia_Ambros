c6a162cc8be7ec9c7823e7fc96f32ba19cf60aa4
Mireia Ambrós Vivancos <mireia.ambros@estudiantat.upc.edu>
2022-06-01 12:16:49 +0200

    Modifications 01-06-2022 (definitivo)

---
+++ separador.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+import pandas as pd
+
+root = "/home/usuaris/imatge/morros/work_fast/mobilitat/ridesafe/barcelona/split_videos/downsampled_low/shared/"
+dest = "/home/usuaris/imatge/mireia.ambros/val2022/shared/"
+
+videos = pd.read_csv("/home/usuaris/imatge/mireia.ambros/validation.csv", sep=' ', header=None, names=['Filename', 'num'])
+#videos.to_csv('prova.csv', index=None, columns=None)
+
+for file_name in os.listdir(root):
+    full_file_name = os.path.join(root, file_name)
+    if os.path.isfile(full_file_name):
+        for row in videos.index:
+            a = videos.loc[row, 'Filename']
+            if a.find(file_name)!=-1:
+                shutil.copy(full_file_name, dest)
